04b01d926a265a0c5164965fded3bb3c3df4ba9c
Thiago Gabriel <thiago.gabriel051@gmail.com>
2020-09-13 23:23:58 -0300

    1 Introdução

---
+++ lib.h
@@ -0,0 +1,11 @@
+/* protótipos 1 Introdução*/
+void init();
+void nextChar();
+void error(char *fmt, ...);
+void fatal(char *fmt, ...);
+void expected(char *fmt, ...);
+void match(char c);
+char getName();
+char getNum();
+void emit(char *fmt, ...);
+/**/
