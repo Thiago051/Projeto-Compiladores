bf570b69035a3e1d7f80fb69cd64361e56c99161
Thiago Gabriel <thiago.gabriel051@gmail.com>
2020-09-13 23:56:35 -0300

    1 Introdução

---
+++ main.c
@@ -0,0 +1,19 @@
+/*
+    Projeto Final de Compiladores
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <ctype.h>
+#include "lib.h"
+
+
+/* PROGRAMA PRINCIPAL */
+int main()
+{
+    init();
+
+    return 0;
+}
+
