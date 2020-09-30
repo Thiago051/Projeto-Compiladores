2b622a40f19dc0bbf4a9cb21c816bfd66f9ac533
Thiago Gabriel <thiago.gabriel051@gmail.com>
2020-09-30 00:34:53 -0300

    12 Miscelâneas

---
+++ capitulo_12/main.c
@@ -0,0 +1,27 @@
+/*
+    Projeto Final de Compiladores
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <ctype.h>
+#include "lib.h"
+
+/* PROGRAMA PRINCIPAL */
+int main()
+{
+    init();
+
+    matchstring("PROGRAM");
+    semicolon();
+    header();
+    topdecls();
+    matchstring("BEGIN");
+    prolog();
+    block();
+    matchstring("END");
+    epilog();
+
+    return 0;
+}
